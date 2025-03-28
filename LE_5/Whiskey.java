ed391c27337cc6977047477a78c070905e42491e
221B124 <221B124@juet.co.in>
2025-03-28 09:17:39 +0530

    My Sixteh Commit

---
+++ LE_5/Whiskey.java
@@ -0,0 +1,8 @@
+class Whiskey extends Beverage{
+    protected void addCondiment(){
+        System.out.println("Add ice.");
+    }
+    protected void stir(){
+        System.out.println("stir for 29 sec.");
+    }
+}
